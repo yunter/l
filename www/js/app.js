d36104dc1c6c42a839ca2e31eb6d93b285198140
yunter <yunter@gmail.com>
2016-08-24 00:24:41 +0800

    new add

--- www/js/app.js
+++ www/js/app.js
@@ -7,162 +7,215 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
+            .state('tab.dash-intro', {
+                url: '/intro',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/dash-intro.html',
+                        controller: 'DashIntroCtrl'
+                    }
+                }
+            })
+            .state('tab.dash-hotProducts', {
+                url: '/hotProducts',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/dash-hotProducts.html',
+                        controller: 'DashHotProductsCtrl'
+                    }
+                }
+            })
+            .state('tab.hot-product-detail', {
+                url: '/hot-product-detail/:productId',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/product-detail.html',
+                        controller: 'ProductDetailCtrl'
+                    }
+                }
+            })
+
+            .state('tab.products', {
+                url: '/products',
+                views: {
+                    'tab-products': {
+                        templateUrl: 'templates/tab-products.html',
+                        controller: 'ProductsCtrl'
+                    }
+                }
+            })
+            .state('tab.product-list', {
+                url: '/product-list',
+                views: {
+                    'tab-products': {
+                        templateUrl: 'templates/product-list.html',
+                        controller: 'ProductsCtrl'
+                    }
+                }
+            })
+            .state('tab.product-video-list', {
+                url: '/product-video-list',
+                views: {
+                    'tab-products': {
+                        templateUrl: 'templates/product-video-list.html',
+                        controller: 'ProductsCtrl'
+                    }
+                }
+            })
+            .state('tab.product-detail', {
+                url: '/product-detail/:productId',
+                views: {
+                    'tab-products': {
+                        templateUrl: 'templates/product-detail.html',
+                        controller: 'ProductDetailCtrl'
+                    }
+                }
+            })
+
+            .state('tab.feedback', {
+                url: '/feedback',
+                views: {
+                    'tab-feedback': {
+                        templateUrl: 'templates/tab-feedback.html',
+                        controller: 'FeedbackCtrl'
+                    }
+                }
+            })
+            .state('tab.feedback-detail', {
+                url: '/feedback/:feedbackId',
+                views: {
+                    'tab-feedback': {
+                        templateUrl: 'templates/feedback-detail.html',
+                        controller: 'FeedbackDetailCtrl'
+                    }
+                }
+            })
+            .state('tab.feedback-history', {
+                url: '/feedback-history',
+                views: {
+                    'tab-feedback': {
+                        templateUrl: 'templates/feedback-history.html',
+                        controller: 'FeedbackCtrl'
+                    }
+                }
+            })
+            .state('tab.feedback-attachments', {
+                url: '/feedback-attachments',
+                views: {
+                    'tab-feedback': {
+                        templateUrl: 'templates/feedback-attachments.html',
+                        controller: 'AttachmentsCtrl'
+                    }
+                }
+            })
+            .state('tab.feedback-address', {
+                url: '/feedback-address',
+                views: {
+                    'tab-feedback': {
+                        templateUrl: 'templates/feedback-address.html',
+                        controller: 'AddressCtrl'
+                    }
+                }
+            })
+
+            .state('tab.account', {
+                url: '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+            .state('tab.account-avatar', {
+                url: '/account-avatar',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/account-avatar.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+            .state('tab.account-username', {
+                url: '/account-username',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/account-username.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+            .state('tab.account-address', {
+                url: '/account-address',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/account-address.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+            .state('tab.account-language', {
+                url: '/account-language',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/account-language.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+            .state('tab.account-about', {
+                url: '/account-about',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/account-about.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/dash');
+
     });
-  })
-
-  .config(function ($stateProvider, $urlRouterProvider) {
-
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-
-    // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('tab.dash', {
-        url: '/dash',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/tab-dash.html',
-            controller: 'DashCtrl'
-          }
-        }
-      })
-      .state('tab.dash-intro', {
-        url: '/intro',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/dash-intro.html',
-            controller: 'DashIntroCtrl'
-          }
-        }
-      })
-      .state('tab.dash-hotProducts', {
-        url: '/hotProducts',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/dash-hotProducts.html',
-            controller: 'DashHotProductsCtrl'
-          }
-        }
-      })
-      .state('tab.hot-product-detail', {
-        url: '/hot-product-detail/:productId',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/product-detail.html',
-            controller: 'ProductDetailCtrl'
-          }
-        }
-      })
-
-      .state('tab.products', {
-        url: '/products',
-        views: {
-          'tab-products': {
-            templateUrl: 'templates/tab-products.html',
-            controller: 'ProductsCtrl'
-          }
-        }
-      })
-      .state('tab.product-list', {
-        url: '/product-list',
-        views: {
-          'tab-products': {
-            templateUrl: 'templates/product-list.html',
-            controller: 'ProductsCtrl'
-          }
-        }
-      })
-      .state('tab.product-video-list', {
-        url: '/product-video-list',
-        views: {
-          'tab-products': {
-            templateUrl: 'templates/product-video-list.html',
-            controller: 'ProductsCtrl'
-          }
-        }
-      })
-      .state('tab.product-detail', {
-        url: '/product-detail/:productId',
-        views: {
-          'tab-products': {
-            templateUrl: 'templates/product-detail.html',
-            controller: 'ProductDetailCtrl'
-          }
-        }
-      })
-
-
-      .state('tab.feedback', {
-        url: '/feedback',
-        views: {
-          'tab-feedback': {
-            templateUrl: 'templates/tab-feedback.html',
-            controller: 'FeedbackCtrl'
-          }
-        }
-      })
-      .state('tab.feedback-detail', {
-        url: '/feedback/:feedbackId',
-        views: {
-          'tab-feedback': {
-            templateUrl: 'templates/feedback-detail.html',
-            controller: 'FeedbackDetailCtrl'
-          }
-        }
-      })
-      .state('tab.feedback-attachments', {
-        url: '/feedback-attachments',
-        views: {
-          'tab-feedback': {
-            templateUrl: 'templates/feedback-attachments.html',
-            controller: 'AttachmentsCtrl'
-          }
-        }
-      })
-      .state('tab.feedback-history', {
-        url: '/feedback-history',
-        views: {
-          'tab-feedback': {
-            templateUrl: 'templates/feedback-history.html',
-            controller: 'FeedbackCtrl'
-          }
-        }
-      })
-
-      .state('tab.account', {
-        url: '/account',
-        views: {
-          'tab-account': {
-            templateUrl: 'templates/tab-account.html',
-            controller: 'AccountCtrl'
-          }
-        }
-      });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
-
-  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,122 +1,154 @@
 angular.module('starter.controllers', ['ngCordova'])
-.controller('AppCtrl', function($scope) {
-  $scope.platform = ionic.Platform.platform();
-  $scope.home     = 'Home';
-  $scope.about_us = 'About Us';
-})
-.controller('DashCtrl', function($scope) {
-})
-.controller('DashIntroCtrl', function($scope) {})
-.controller('DashHotProductsCtrl', function($scope, $http) {
-  $scope.items = [1,2,3];
-  $scope.doRefresh = function() {
-    $http.get('/')
-      .success(function(newItems) {
-        $scope.items = newItems;
-      })
-      .finally(function() {
-        // Stop the ion-refresher from spinning
-        $scope.$broadcast('scroll.refreshComplete');
-      });
-  };
-})
-.controller('ProductsCtrl', function($scope) {
-  $scope.productId = 1;
-  $scope.items = [1,2,3];
-})
-.controller('ProductDetailCtrl', function($scope, $stateParams, Products) {
-  $scope.product = Products.get($stateParams.productId);
-})
+    .controller('AppCtrl', function ($scope) {
+        $scope.platform = ionic.Platform.platform();
+        $scope.home = 'Home';
+        $scope.about_us = 'About Us';
+    })
+    .controller('DashCtrl', function ($scope) {
+    })
+    .controller('DashIntroCtrl', function ($scope) {
+    })
+    .controller('DashHotProductsCtrl', function ($scope, $http) {
+        $scope.items = [1, 2, 3];
+        $scope.doRefresh = function () {
+            $http.get('/')
+                .success(function (newItems) {
+                    $scope.items = newItems;
+                })
+                .finally(function () {
+                    // Stop the ion-refresher from spinning
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        };
+    })
+    .controller('ProductsCtrl', function ($scope) {
+        $scope.productId = 1;
+        $scope.items = [1, 2, 3];
+    })
+    .controller('ProductDetailCtrl', function ($scope, $stateParams, Products) {
+        $scope.product = Products.get($stateParams.productId);
+    })
 
-.controller('FeedbackCtrl', function($scope, $ionicActionSheet, $timeout, Feedback) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('FeedbackCtrl', function ($scope, $ionicActionSheet, $timeout, Feedback) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  $scope.feedback = Feedback.all();
-  $scope.remove = function(feedback) {
-    Feedback.remove(feedback);
-  };
-})
+        $scope.feedback = Feedback.all();
+        $scope.remove = function (feedback) {
+            Feedback.remove(feedback);
+        };
+    })
 
-.controller('AttachmentsCtrl', function($scope, $stateParams, $ionicActionSheet, $timeout) {
-  // Triggered on a button click, or some other target
-  $scope.show = function() {
+    .controller('AttachmentsCtrl', function ($scope, $stateParams, $ionicActionSheet, $timeout) {
+        // Triggered on a button click, or some other target
+        $scope.show = function () {
 
-    // Show the action sheet
-    var hideSheet = $ionicActionSheet.show({
-      buttons: [
-        { text: 'Choose form album' },
-        { text: 'Take Photos' }
-        ],
-      //destructiveText: 'Delete',
-      titleText: 'Choose you action',
-      cancelText: 'Cancel',
-      cancel: function() {
-        // add cancel code..
-      },
-      buttonClicked: function(index) {
-        switch (index){
-          case 0:
-            alert('test');
-            break;
-          default:
-            alert(index);
-            break;
+            // Show the action sheet
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: 'Choose form album'},
+                    {text: 'Take Photos'}
+                ],
+                //destructiveText: 'Delete',
+                titleText: 'Choose you action',
+                cancelText: 'Cancel',
+                cancel: function () {
+                    // add cancel code..
+                },
+                buttonClicked: function (index) {
+                    switch (index) {
+                        case 0:
+                            alert('test');
+                            break;
+                        default:
+                            alert(index);
+                            break;
+                    }
+                    return true;
+                }
+            });
+
+            // For example's sake, hide the sheet after two seconds
+            $timeout(function () {
+                hideSheet();
+            }, 2500);
+
+        };
+        $scope.showCam = function () {
+
+            // Show the action sheet
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: 'Choose form album'},
+                    {text: 'Take Video'}
+                ],
+                //destructiveText: 'Delete',
+                titleText: 'Choose you action',
+                cancelText: 'Cancel',
+                cancel: function () {
+                    // add cancel code..
+                },
+                buttonClicked: function (index) {
+                    switch (index) {
+                        case 0:
+                            alert('test');
+                            break;
+                        default:
+                            alert(index);
+                            break;
+                    }
+                    return true;
+                }
+            });
+
+            // For example's sake, hide the sheet after two seconds
+            $timeout(function () {
+                hideSheet();
+            }, 2500);
+
+        };
+    })
+    .controller('AddressCtrl', function ($scope) {
+    })
+    .controller('FeedbackDetailCtrl', function ($scope, $stateParams, Feedback) {
+        $scope.feedback = Feedback.get($stateParams.feedbackId);
+    })
+
+    .controller('AccountCtrl', function ($scope) {
+    })
+
+    .controller("IntroBoxCtrl", function ($scope) {
+        //var ctrl = this;
+
+        //ctrl.showIntro = function () {
+        //angular.element(document.getElementById('intro-box')).addClass("intro-box-full");
+        //}
+        $scope.showIntro = function () {
+            return 'yes';
         }
-        return true;
-      }
+    })
+    .directive('hideTabs', function ($rootScope) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attributes) {
+                scope.$on('$ionicView.beforeEnter', function () {
+                    scope.$watch(attributes.hideTabs, function (value) {
+                        $rootScope.hideTabs = 'tabs-item-hide';
+                    });
+                });
+                scope.$on('$ionicView.beforeLeave', function () {
+                    scope.$watch(attributes.hideTabs, function (value) {
+                        $rootScope.hideTabs = 'tabs-item-hide';
+                    });
+                    scope.$watch('$destroy', function () {
+                        $rootScope.hideTabs = false;
+                    })
+                });
+            }
+        };
     });
-
-    // For example's sake, hide the sheet after two seconds
-    $timeout(function() {
-      hideSheet();
-    }, 2500);
-
-  };
-})
-
-.controller('FeedbackDetailCtrl', function($scope, $stateParams, Feedback) {
-  $scope.feedback = Feedback.get($stateParams.feedbackId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
-
-.controller("IntroBoxCtrl", function ($scope) {
-  //var ctrl = this;
-
-  //ctrl.showIntro = function () {
-    //angular.element(document.getElementById('intro-box')).addClass("intro-box-full");
-  //}
-  $scope.showIntro = function () {
-    return 'yes';
-  }
-})
-.directive('hideTabs', function ($rootScope) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attributes) {
-      scope.$on('$ionicView.beforeEnter', function () {
-        scope.$watch(attributes.hideTabs, function (value) {
-          $rootScope.hideTabs = 'tabs-item-hide';
-        });
-      });
-      scope.$on('$ionicView.beforeLeave', function () {
-        scope.$watch(attributes.hideTabs, function (value) {
-          $rootScope.hideTabs = 'tabs-item-hide';
-        });
-        scope.$watch('$destroy', function () {
-          $rootScope.hideTabs = false;
-        })
-      });
-    }
-  };
-});
