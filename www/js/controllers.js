045a4bd57e02b61ec16f99e9a8d774de53405474
Yunter <yunter@gmail.com>
2016-08-25 20:12:46 +0800

    post

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -131,24 +131,4 @@ angular.module('starter.controllers', ['ngCordova'])
         $scope.showIntro = function () {
             return 'yes';
         }
-    })
-    .directive('hideTabs', function ($rootScope) {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attributes) {
-                scope.$on('$ionicView.beforeEnter', function () {
-                    scope.$watch(attributes.hideTabs, function (value) {
-                        $rootScope.hideTabs = 'tabs-item-hide';
-                    });
-                });
-                scope.$on('$ionicView.beforeLeave', function () {
-                    scope.$watch(attributes.hideTabs, function (value) {
-                        $rootScope.hideTabs = 'tabs-item-hide';
-                    });
-                    scope.$watch('$destroy', function () {
-                        $rootScope.hideTabs = false;
-                    })
-                });
-            }
-        };
     });
