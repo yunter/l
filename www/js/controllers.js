7e1933a8b434f94ecf816eb3f5f59044fe1e5721
Yunter <yunter@gmail.com>
2016-09-09 19:17:51 +0800

    push data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,143 +1,168 @@
 angular.module('starter.controllers', ['ngCordova'])
-    .controller('AppCtrl', function ($scope) {
-        $scope.platform = ionic.Platform.platform();
-        $scope.home = 'Home';
-        $scope.about_us = 'About Us';
-    })
-    .controller('DashCtrl', function ($scope, ApiHome) {
-        ApiHome.getSlideShow().then(function (result) {
-            if(result.banners != undefined) {
-                $scope.banners = result.banners;
+  .controller('AppCtrl', function ($scope) {
+    $scope.platform = ionic.Platform.platform();
+    $scope.home = 'Home';
+    $scope.about_us = 'About Us';
+  })
+  .controller('DashCtrl', function ($scope, ApiHome) {
+    ApiHome.getLamsin().then(function (result) {
+      if (typeof result == "object") {
+
+        //banner
+        ApiHome.getSlideShow(result.bannerConfig.id, result.bannerConfig.width, result.bannerConfig.height)
+          .then(function (result) {
+            if (result.data.banners != undefined) {
+              $scope.banners = result.data.banners;
             }
-        }, function (error) {
+          }, function (error) {
             console.log(error);
-        }, function (msg) {
+          }, function (msg) {
             console.log(msg);
-        });
-    })
-    .controller('DashIntroCtrl', function ($scope) {
-    })
-    .controller('DashHotProductsCtrl', function ($scope, $http) {
-        $scope.items = [1, 2, 3];
-        $scope.doRefresh = function () {
-            $http.get('/')
-                .success(function (newItems) {
-                    $scope.items = newItems;
-                })
-                .finally(function () {
-                    // Stop the ion-refresher from spinning
-                    $scope.$broadcast('scroll.refreshComplete');
-                });
-        };
-    })
-    .controller('ProductsCtrl', function ($scope) {
-        $scope.productId = 1;
-        $scope.items = [1, 2, 3];
-    })
-    .controller('ProductDetailCtrl', function ($scope, $stateParams, Products) {
-        $scope.product = Products.get($stateParams.productId);
-    })
+          });
 
-    .controller('FeedbackCtrl', function ($scope, $ionicActionSheet, $timeout, Feedback) {
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
+        //intro
+        ApiHome.getIntro(result.articleConfig.intro)
+          .then(function (result) {
+            if (typeof result == "object") {
+              $scope.IntroTitle = result.data.title;
+              $scope.IntroDesc  = result.data.meta_description;
+            }
+          }, function (error) {
+            console.log(error);
+          }, function (msg) {
+            console.log(msg);
+          });
 
-        $scope.feedback = Feedback.all();
-        $scope.remove = function (feedback) {
-            Feedback.remove(feedback);
-        };
-    })
-
-    .controller('AttachmentsCtrl', function ($scope, $stateParams, $ionicActionSheet, $timeout) {
-        // Triggered on a button click, or some other target
-        $scope.show = function () {
-
-            // Show the action sheet
-            var hideSheet = $ionicActionSheet.show({
-                buttons: [
-                    {text: 'Choose form album'},
-                    {text: 'Take Photos'}
-                ],
-                //destructiveText: 'Delete',
-                titleText: 'Choose you action',
-                cancelText: 'Cancel',
-                cancel: function () {
-                    // add cancel code..
-                },
-                buttonClicked: function (index) {
-                    switch (index) {
-                        case 0:
-                            alert('test');
-                            break;
-                        default:
-                            alert(index);
-                            break;
-                    }
-                    return true;
-                }
-            });
-
-            // For example's sake, hide the sheet after two seconds
-            $timeout(function () {
-                hideSheet();
-            }, 2500);
-
-        };
-        $scope.showCam = function () {
-
-            // Show the action sheet
-            var hideSheet = $ionicActionSheet.show({
-                buttons: [
-                    {text: 'Choose form album'},
-                    {text: 'Take Video'}
-                ],
-                //destructiveText: 'Delete',
-                titleText: 'Choose you action',
-                cancelText: 'Cancel',
-                cancel: function () {
-                    // add cancel code..
-                },
-                buttonClicked: function (index) {
-                    switch (index) {
-                        case 0:
-                            alert('test');
-                            break;
-                        default:
-                            alert(index);
-                            break;
-                    }
-                    return true;
-                }
-            });
-
-            // For example's sake, hide the sheet after two seconds
-            $timeout(function () {
-                hideSheet();
-            }, 2500);
-
-        };
-    })
-    .controller('AddressCtrl', function ($scope) {
-    })
-    .controller('FeedbackDetailCtrl', function ($scope, $stateParams, Feedback) {
-        $scope.feedback = Feedback.get($stateParams.feedbackId);
-    })
-
-    .controller('AccountCtrl', function ($scope) {
-    })
-
-    .controller("IntroBoxCtrl", function ($scope) {
-        //var ctrl = this;
-
-        //ctrl.showIntro = function () {
-        //angular.element(document.getElementById('intro-box')).addClass("intro-box-full");
-        //}
-        $scope.showIntro = function () {
-            return 'yes';
-        }
+      }
+    }, function (error) {
+      console.log(error);
+    }, function (msg) {
+      console.log(msg);
     });
+  })
+  .controller('DashIntroCtrl', function ($scope) {
+  })
+  .controller('DashHotProductsCtrl', function ($scope, ApiHome) {
+    //hot products
+    ApiHome.getLatestProducts(3).then(
+      function (result) {
+        if (typeof result == "object") {
+          $scope.latestProducts = result.data;
+        }
+      }, function (error) {
+        console.log(error);
+      }, function (msg) {
+        console.log(msg);
+      });
+  })
+  .controller('ProductsCtrl', function ($scope) {
+    $scope.productId = 1;
+    $scope.items = [1, 2, 3];
+  })
+  .controller('ProductDetailCtrl', function ($scope, $stateParams, Products) {
+    $scope.product = Products.get($stateParams.productId);
+  })
+
+  .controller('FeedbackCtrl', function ($scope, $ionicActionSheet, $timeout, Feedback) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.feedback = Feedback.all();
+    $scope.remove = function (feedback) {
+      Feedback.remove(feedback);
+    };
+  })
+
+  .controller('AttachmentsCtrl', function ($scope, $stateParams, $ionicActionSheet, $timeout) {
+    // Triggered on a button click, or some other target
+    $scope.show = function () {
+
+      // Show the action sheet
+      var hideSheet = $ionicActionSheet.show({
+        buttons: [
+          {text: 'Choose form album'},
+          {text: 'Take Photos'}
+        ],
+        //destructiveText: 'Delete',
+        titleText: 'Choose you action',
+        cancelText: 'Cancel',
+        cancel: function () {
+          // add cancel code..
+        },
+        buttonClicked: function (index) {
+          switch (index) {
+            case 0:
+              alert('test');
+              break;
+            default:
+              alert(index);
+              break;
+          }
+          return true;
+        }
+      });
+
+      // For example's sake, hide the sheet after two seconds
+      $timeout(function () {
+        hideSheet();
+      }, 2500);
+
+    };
+    $scope.showCam = function () {
+
+      // Show the action sheet
+      var hideSheet = $ionicActionSheet.show({
+        buttons: [
+          {text: 'Choose form album'},
+          {text: 'Take Video'}
+        ],
+        //destructiveText: 'Delete',
+        titleText: 'Choose you action',
+        cancelText: 'Cancel',
+        cancel: function () {
+          // add cancel code..
+        },
+        buttonClicked: function (index) {
+          switch (index) {
+            case 0:
+              alert('test');
+              break;
+            default:
+              alert(index);
+              break;
+          }
+          return true;
+        }
+      });
+
+      // For example's sake, hide the sheet after two seconds
+      $timeout(function () {
+        hideSheet();
+      }, 2500);
+
+    };
+  })
+  .controller('AddressCtrl', function ($scope) {
+  })
+  .controller('FeedbackDetailCtrl', function ($scope, $stateParams, Feedback) {
+    $scope.feedback = Feedback.get($stateParams.feedbackId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+  })
+
+  .controller("IntroBoxCtrl", function ($scope) {
+    //var ctrl = this;
+
+    //ctrl.showIntro = function () {
+    //angular.element(document.getElementById('intro-box')).addClass("intro-box-full");
+    //}
+    $scope.showIntro = function () {
+      return 'yes';
+    }
+  });
--- www/js/services.js
+++ www/js/services.js
@@ -1,185 +1,297 @@
 angular.module('starter.services', [])
-    .constant('ApiHost', {
-        url: 'http://lamsin.bluemorpho.cn/index.php?route=openapi'
-    })
-    .factory('ApiRegDevice', function ($http, $q, $httpParamSerializer, ApiHost) {
-        function regDevice(uuid, Platform, sysVersion, AppVersion) {
-            try {
-                var request = {
-                    method: 'POST',
-                    url: ApiHost.url + '/account/registerdevice',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        //'Deviceid': 'deviceid',
-                        //'Token': 'token',
-                        'Sysversion': sysVersion,
-                        'Sysname': Platform
-                    },
-                    data: $httpParamSerializer({
-                        uuid: uuid,
-                        appversion: AppVersion,
-                        sysversion: sysVersion,
-                        sysname: Platform
-                    })
-                };
-              var deferred = $q.defer();       // This will handle your promise
-
-              $http(request).then(function (response) {
-                if (typeof response.data === 'object') {
-                  deferred.resolve(response.data);
-                } else {
-                  deferred.reject(response.data);
-                }
-              }, function (error) {
-                return deferred.error(error.data);
-              });
-
-              return deferred.promise;
-
-            } catch (err) {
-                return err;
-            }
-        }
-
-        return {
-          regDevice: function (uuid, Platform, sysVersion, AppVersion) {
-                return regDevice(uuid, Platform, sysVersion, AppVersion);
-            }
-        }
-    })
-
-    .factory('ApiHome', function ($http, $q, ApiHost) {
-        function getSlideShow() {
-            try {
-                var request = {
-                    method: 'POST',
-                    url: ApiHost.url + '/slideshow/getshow',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        //'Deviceid': 'deviceid',
-                        //'Token': 'token'
-                    }
-                };
-              var deferred = $q.defer();       // This will handle your promise
-
-              $http(request).then(function (response) {
-                if (typeof response.data === 'object') {
-                  deferred.resolve(response.data);
-                } else {
-                  deferred.reject(response.data);
-                }
-              }, function (error) {
-                return deferred.error(error.data);
-              });
-
-              return deferred.promise;
-
-            } catch (err) {
-                return err;
-            }
-        }
-
-        return {
-          getSlideShow: function () {
-                return getSlideShow();
-            }
-        }
-    })
-
-    .factory('Feedback', function () {
-        // Might use a resource here that returns a JSON array
-
-        // Some fake testing data
-        var feedback = [{
-            id: 0,
-            name: 'Ben Sparrow',
-            lastText: 'You on your way?',
-            face: 'img/ben.png'
-        }, {
-            id: 1,
-            name: 'Max Lynx',
-            lastText: 'Hey, it\'s me',
-            face: 'img/max.png'
-        }, {
-            id: 2,
-            name: 'Adam Bradleyson',
-            lastText: 'I should buy a boat',
-            face: 'img/adam.jpg'
-        }, {
-            id: 3,
-            name: 'Perry Governor',
-            lastText: 'Look at my mukluks!',
-            face: 'img/perry.png'
-        }, {
-            id: 4,
-            name: 'Mike Harrington',
-            lastText: 'This is wicked good ice cream.',
-            face: 'img/mike.png'
-        }];
-
-        return {
-            all: function () {
-                return feedback;
-            },
-            remove: function (feedback) {
-                feedback.splice(feedback.indexOf(feedback), 1);
-            },
-            get: function (feedbackId) {
-                for (var i = 0; i < feedback.length; i++) {
-                    if (feedback[i].id === parseInt(feedbackId)) {
-                        return feedback[i];
-                    }
-                }
-                return null;
-            }
+  .constant('ApiHost', {
+    domain: 'http://ok.cms.debug',
+    uri: '/index.php?route=openapi'
+  })
+  .factory('ApiRegDevice', function ($http, $q, $httpParamSerializer, ApiHost) {
+    function regDevice(uuid, Platform, sysVersion, AppVersion) {
+      try {
+        var request = {
+          method: 'POST',
+          url: ApiHost.domain + ApiHost.uri + '/account/registerdevice',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            //'Deviceid': 'deviceid',
+            //'Token': 'token',
+            'Sysversion': sysVersion,
+            'Sysname': Platform
+          },
+          data: $httpParamSerializer({
+            uuid: uuid,
+            appversion: AppVersion,
+            sysversion: sysVersion,
+            sysname: Platform
+          })
         };
-    })
+        var deferred = $q.defer();       // This will handle your promise
 
-    .factory('Products', function () {
-        // Might use a resource here that returns a JSON array
+        $http(request).then(function (response) {
+          if (typeof response.data === 'object') {
+            deferred.resolve(response.data);
+          } else {
+            deferred.reject(response.data);
+          }
+        }, function (error) {
+          return deferred.reject(error.data);
+        });
 
-        // Some fake testing data
-        var products = [{
-            id: 0,
-            name: 'Ben Sparrow',
-            lastText: 'You on your way?',
-            face: 'img/ben.png'
-        }, {
-            id: 1,
-            name: 'Max Lynx',
-            lastText: 'Hey, it\'s me',
-            face: 'img/max.png'
-        }, {
-            id: 2,
-            name: 'Adam Bradleyson',
-            lastText: 'I should buy a boat',
-            face: 'img/adam.jpg'
-        }, {
-            id: 3,
-            name: 'Perry Governor',
-            lastText: 'Look at my mukluks!',
-            face: 'img/perry.png'
-        }, {
-            id: 4,
-            name: 'Mike Harrington',
-            lastText: 'This is wicked good ice cream.',
-            face: 'img/mike.png'
-        }];
+        return deferred.promise;
 
-        return {
-            all: function () {
-                return products;
-            },
-            remove: function (feedback) {
-                products.splice(products.indexOf(feedback), 1);
-            },
-            get: function (productId) {
-                for (var i = 0; i < products.length; i++) {
-                    if (products[i].id === parseInt(productId)) {
-                        return products[i];
-                    }
-                }
-                return null;
-            }
+      } catch (err) {
+        return err;
+      }
+    }
+
+    return {
+      regDevice: function (uuid, Platform, sysVersion, AppVersion) {
+        return regDevice(uuid, Platform, sysVersion, AppVersion);
+      }
+    }
+  })
+
+  .factory('ApiHome', function ($http, $q, $httpParamSerializer, ApiHost) {
+
+    function getLamsin() {
+      try {
+        var request = {
+          method: 'GET',
+          url: ApiHost.domain + '/lamsin.json',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            //'Deviceid': 'deviceid',
+            //'Token': 'token'
+          }
         };
-    });
+        var deferred = $q.defer();       // This will handle your promise
+
+        $http(request).then(function (response) {
+          if (typeof response.data === 'object') {
+            deferred.resolve(response.data);
+          } else {
+            deferred.reject(response.data);
+          }
+        }, function (error) {
+          return deferred.reject(error.data);
+        });
+
+        return deferred.promise;
+
+      } catch (err) {
+        return err;
+      }
+    }
+
+    function getSlideShow(bannerId, bWidth, bHeight) {
+      try {
+        var request = {
+          method: 'POST',
+          url: ApiHost.domain + ApiHost.uri + '/slideshow/getshow',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            //'Deviceid': 'deviceid',
+            //'Token': 'token'
+          },
+          data: $httpParamSerializer({
+            bannerId: bannerId,
+            bWidth: bWidth,
+            bHeight:bHeight
+          })
+
+        };
+        var deferred = $q.defer();       // This will handle your promise
+
+        $http(request).then(function (response) {
+          if (typeof response.data === 'object') {
+            deferred.resolve(response.data);
+          } else {
+            deferred.reject(response.data);
+          }
+        }, function (error) {
+          return deferred.reject(error.data);
+        });
+
+        return deferred.promise;
+
+      } catch (err) {
+        return err;
+      }
+    }
+
+    function getIntro(articleId) {
+      try {
+        var request = {
+          method: 'POST',
+          url: ApiHost.domain + ApiHost.uri + '/articles/getArticle',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            //'Deviceid': 'deviceid',
+            //'Token': 'token'
+          },
+          data: $httpParamSerializer({
+            article_id: articleId,
+          })
+        };
+        var deferred = $q.defer();       // This will handle your promise
+
+        $http(request).then(function (response) {
+          if (typeof response.data === 'object') {
+            deferred.resolve(response.data);
+          } else {
+            deferred.reject(response.data);
+          }
+        }, function (error) {
+          return deferred.reject(error.data);
+        });
+
+        return deferred.promise;
+
+      } catch (err) {
+        return err;
+      }
+    }
+
+    function getLatestProducts(limit) {
+      try {
+        var request = {
+          method: 'POST',
+          url: ApiHost.domain + ApiHost.uri + '/products/getLatestProducts',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            //'Deviceid': 'deviceid',
+            //'Token': 'token'
+          },
+          data: $httpParamSerializer({
+            limit: limit,
+          })
+        };
+        var deferred = $q.defer();       // This will handle your promise
+        $http(request).then(function (response) {
+          if (typeof response.data === 'object') {
+            deferred.resolve(response.data);
+          } else {
+            deferred.reject(response.data);
+          }
+        }, function (error) {
+          return deferred.reject(error.data);
+        });
+
+        return deferred.promise;
+
+      } catch (err) {
+        return err;
+      }
+    }
+
+    return {
+      getLamsin: function () {
+        return getLamsin();
+      },
+      getSlideShow: function (bannerId, bWidth, bHeight) {
+        return getSlideShow(bannerId, bWidth, bHeight);
+      },
+      getIntro: function (articleId) {
+        return getIntro(articleId);
+      },
+      getLatestProducts: function (limit) {
+        return getLatestProducts(limit);
+      }
+    }
+  })
+
+  .factory('Feedback', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var feedback = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }];
+
+    return {
+      all: function () {
+        return feedback;
+      },
+      remove: function (feedback) {
+        feedback.splice(feedback.indexOf(feedback), 1);
+      },
+      get: function (feedbackId) {
+        for (var i = 0; i < feedback.length; i++) {
+          if (feedback[i].id === parseInt(feedbackId)) {
+            return feedback[i];
+          }
+        }
+        return null;
+      }
+    };
+  })
+
+  .factory('Products', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var products = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }];
+
+    return {
+      all: function () {
+        return products;
+      },
+      remove: function (feedback) {
+        products.splice(products.indexOf(feedback), 1);
+      },
+      get: function (productId) {
+        for (var i = 0; i < products.length; i++) {
+          if (products[i].id === parseInt(productId)) {
+            return products[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
